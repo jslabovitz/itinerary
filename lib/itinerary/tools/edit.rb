4d5446baa03190542755ab89bb37c5e9257a4221
John Labovitz <johnl@johnlabovitz.com>
2013-03-11 10:44:16 -0400

    Skip records that haven't been changed in editing. Show diff of changed records.

--- lib/itinerary/tools/edit.rb
+++ lib/itinerary/tools/edit.rb
@@ -11,12 +11,16 @@ class Itinerary
 
     def run
       @recs.each do |rec|
+        old_rec = rec.dup
         rec.edit(:wait => true)
         rec = Record.load(rec.path)
+        if rec == old_rec
+          warn "Unchanged"
+          next
+        end
+        rec.print_diff(old_rec)
         unless rec.geocoded?
-          rec.geocode or begin
-            raise "Failed to geocode #{rec.address.inspect} (entry left in #{rec.path})"
-          end
+          rec.geocode or raise "Failed to geocode #{rec.address.inspect} (entry left in #{rec.path})"
           rec.save!
         end
         new_path = rec.make_path(@itinerary.entries_path)
