a975013141b9a0e5a608732b1a4b248ce063616b
John Labovitz <johnl@johnlabovitz.com>
2014-05-30 11:14:57 -0400

    In text view, add separators between printed records.

--- lib/itinerary/views/text.rb
+++ lib/itinerary/views/text.rb
@@ -5,6 +5,8 @@ class Itinerary
       def render_record(rec)
         @output.puts "[#{rec.path.relative_path_from(@itinerary.entries_path)}]"
         @output.puts rec.to_text(:field_keys => @field_keys)
+        @output.puts '---'
+        @output.puts ''
       end
 
     end
